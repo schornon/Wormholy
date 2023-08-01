e09e9f86c185df86b90bd7d46d68d1ae3c881034
Chornonoh Serhii <cs.hilfy@gmail.com>
2023-08-01 11:28:16 +0300

    Package.swift - path to Sources was added.

--- Package.swift
+++ Package.swift
@@ -20,9 +20,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Wormholy",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources",
+            exclude: ["Objc"]),
         .testTarget(
             name: "WormholyTests",
-            dependencies: ["Wormholy"]),
+            dependencies: ["Wormholy"],
+            path: "Tests"),
     ]
 )
